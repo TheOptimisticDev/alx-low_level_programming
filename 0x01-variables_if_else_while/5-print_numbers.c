7d68152d230ab10cd2137c63518bbae14a750ae0
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-12 13:46:13 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints the number since 0 to 9
  *
- * Return: Always 0 (success)
+ * Return: Always (success)
  */
 int main(void)
 {
-	int a;
+	char n;
 	
-	for(a = 0; a <= 10; a++)
-		putchar(a);
-
+	for(n = '0'; a <= '9'; n++)
+	{
+		putchar(n);
+	}
 	putchar('\n');
 	return(0);
 }
