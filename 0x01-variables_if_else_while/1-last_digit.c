a53d9ffa493c4df0e8989bb8134adb04fce93964
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-12 03:17:04 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,4 +22,6 @@ int main(void)
 		printf("%s %d is %d and is 0\n", str, n, L);
 	else if (L < 6)
 		printf("%s %dd is %d and is less than 6  and not 0\n", str, n, L);
-	return (0):
+	return (0) : 
+}
+
