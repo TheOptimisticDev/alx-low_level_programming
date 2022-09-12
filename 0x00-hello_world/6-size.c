bb950865a3e223923aa17091526b45d316ba33ef
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-12 00:48:27 -0700

    C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle"
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
