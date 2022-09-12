b40d5d3802a7f68d628bd549ab97914d9ff4abd0
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-12 12:50:49 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i, j;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 1; j <= 9; j++)
+		for j = 1; j <= 9; j++)
 		{
-			if(j > i)
+			if (j > i)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if(i != 8)
+				if (i != 8)
 				{
 					putchar(',');
 					putchar(' ');
