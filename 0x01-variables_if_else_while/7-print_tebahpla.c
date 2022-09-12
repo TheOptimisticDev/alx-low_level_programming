754b82f6e8a024a6f30743f4864842bc9a63e68d
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-12 11:33:23 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry - point
  *
  * Return: Always 0 (success)
  */
@@ -10,7 +10,7 @@ int main(void)
 
 	c = 'z';
 	while
-		(c >== 'a') {
+		(c >= 'a') {
 			putchar(c);
 			c--;
 		}
