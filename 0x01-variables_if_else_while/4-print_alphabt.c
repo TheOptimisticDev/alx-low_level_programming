19548a056edeb00d81616ecf8882f64f23f546c1
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-12 10:59:47 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,10 @@ int main(void)
 	c = 'a';
 	while
 		(c <= 'z') {
-			if (( c != 'q' || c != 'e') && c <= 'z')
+			if (( c != 'q' || c != 'e')&& c <= 'z')
 				putchar(c);
 			c++;
 		}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
