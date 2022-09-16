50f33fd294785305495a7da6c141463b463fb5cc
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-16 00:01:30 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,17 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	printf("infinite loop incoming :(\n");
+printf("infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 	/*
 	 * while (i < 10)
 	 * {
-	 * putchar(i)
+	 * putchar(i);
 	 * }
 	 */
-	printf("infinite loop avoided! \\o/n");
-
-	return (0);
+printf("infinite loop avoided! \\o/\n");
+return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
