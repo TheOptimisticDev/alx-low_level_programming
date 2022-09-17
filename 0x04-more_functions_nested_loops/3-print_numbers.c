b624a9bf71783768f493543be7b091c0c491fbeb
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-17 08:46:16 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * print_number - Print the numbers since 0 up to 9
+ *
+ * Description: Prints numbers
+ *
+ * Return: The number since 0 up to 9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+}
