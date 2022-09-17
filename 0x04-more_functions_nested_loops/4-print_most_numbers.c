8bc621f9ee7b5bfaa54a4f6e08406f75e1184dde
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-17 05:43:19 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
  * print_most_numbers - print 0 - 9 apart
  *          from 2 and 4 and you can
  *          only use _putchar twice
- *          Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 void print_most_numbers(void)
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; ++num)
 	{
-		if (num == 2 || num ==4)
+		if (num == 2 || num == 4)
 			continue;
 		_putchar(num + 48);
 	}
