7b17bb480be4bc2f4f4e02cc59504f6d180f9547
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-19 23:52:52 -0700

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,19 @@
 #include <string.h>
 /**
  * print_rev - Prints a string in reverse
- * @s: The string to be printed in reverse
- * Return: Nothing
+ * @s: the string to be printed in reverse
  */
 
 void print_rev(char *s);
 {
 	int i, n;
 
-	n = strlen(s);
+	n = 0;
+	while (s[n] != '\0')
+		n++;
 	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
+	_putchar('\n')
 }
