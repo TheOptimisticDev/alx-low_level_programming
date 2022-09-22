0fa0aa49a90d5d6f8259274ba5ff52af2be05aed
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-21 23:21:16 -0700

    1-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- *
- * Decription: header file containing prototype for all functions
- */
-
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
