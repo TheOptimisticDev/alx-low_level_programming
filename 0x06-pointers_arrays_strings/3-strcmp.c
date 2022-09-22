959b9c38e06c6c5d5e56e0f6d52cd66bbcc644e0
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-09-22 06:40:34 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
 /**
  * _strcmp - function which compare two strings
  * @s1: first string
- * @s1: second string
+ * @s2: second string
  * Return: returns zero if s1 ==s2
  *         returns negative number if s1 < s2
  *         returns positive number is s1 > s2
