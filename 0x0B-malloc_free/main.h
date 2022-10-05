87b955929642707f1c541caf34e10997d92761ad
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-10-05 08:52:02 -0700

    hot fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * void prototypes()
- * int prototypes()
- */
-
 int _putchar(char);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
