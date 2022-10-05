97d58369e56e19d38843299e44484fc15a8a5197
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-10-05 08:47:49 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 /**
  * _strdup - returns a pointer to newly allocated space in memory,
  *           which contains a copy of the string given as a parameter.
- * @str: string to be copied
+ * @str: String given as a parameter.
  *
  * Return: NULL in case of error, pointer to allocated space
  */
