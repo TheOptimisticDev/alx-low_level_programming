4d88544b6c4b3fee4e0907159142213fb5dc8cd4
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-10-08 14:18:30 -0700

    hot fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,7 @@
  * void int prototypes
  */
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
