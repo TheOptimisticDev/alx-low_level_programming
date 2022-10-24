0a62a52b7ee7a013fef3320e40a724b1ed7b9937
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-10-24 07:49:53 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * print_listint - ...
+ * @h: list.
+ * Return: The elements linked.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++
+	}
+	return (i);
+}
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - ...
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	listint_t *head;
+	listint_t *new;
+	listint_t hello = {8, NULL};
+	size_t n;
+
+	head = &hello;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->n = 9;
+	new->next = head;
+	head = new;
+	n = print_listint(head);
+	printf("->%lu element\n", n);
+	free(new);
+	return (0);
+}
