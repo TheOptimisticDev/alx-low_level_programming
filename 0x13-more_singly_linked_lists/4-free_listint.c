4f604e1e75759440dec63de8f6e275ca673aece9
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-10-25 13:06:09 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 
 /**
- * free_listint - A function that frees list.
- * @head: head.
+ * free_listint - A function that frees listint_t list.
+ * @head: A pointer to the head of the list_it list.
  */
 
 void free_listint(listint_t *head)
 {
-	listint_t *liberador;
+	listint_t *tmp;
 
-	while (head != NULL)
+	while (head)
 	{
-		liberador = head;
-		head = head->next;
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
