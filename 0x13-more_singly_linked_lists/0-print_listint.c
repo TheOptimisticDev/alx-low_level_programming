b77607813af7f6d99ff697bf4179d2af3b0cb47e
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-10-25 12:27:47 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -18,37 +18,3 @@ size_t print_listint(const listint_t *h)
 	}
 	return (i);
 }
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - ...
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	listint_t *head;
-	listint_t *new;
-	listint_t hello = {8, NULL};
-	size_t n;
-
-	head = &hello;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->n = 9;
-	new->next = head;
-	head = new;
-	n = print_listint(head);
-	printf("->%lu element\n", n);
-	free(new);
-	return (0);
-}
