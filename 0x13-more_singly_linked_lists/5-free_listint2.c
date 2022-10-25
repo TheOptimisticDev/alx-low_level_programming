a6aadb4fe829b620bf1a00e1918719987d607c53
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-10-25 13:15:07 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,7 +4,7 @@
  * free_listint2 - A fuction to free listint_t list.
  * @head: A pointer to the address of the head of the listint_t list.
  *
- * Description: Setsthe head to NULL.
+ * Description: Sets the head to NULL.
  */
 
 void free_listint2(listint_t **head)
