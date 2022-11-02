92ca74362d7b09c0d6a431137cfa1438fe34d8c3
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-11-02 01:24:22 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dog
+ * @d: dog to be freed
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
