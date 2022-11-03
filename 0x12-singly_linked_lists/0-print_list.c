8143023950735afefcfe37ff70b22dbc500098a5
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-11-03 01:42:30 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ *
+ * @h:pointer to the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (i);
+}
