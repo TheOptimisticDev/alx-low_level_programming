e7e829159f888fbfdedb3b483096eee7f1c46e71
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-11-04 01:09:28 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,25 +5,30 @@
 /**
  * print_list - prints all the elements of a list
  *
- * @h:pointer to the list
+ * @h: struct
  *
  * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
-	const list_t *tmp = h;
+	int count = 0;
 
-	for (i = 0; tmp; i++)
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
 	{
-		if (tmp->str == NULL)
-			printf("[0] (nil)\n");
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
 		{
-			printf("[%d] %s\n", tmp->len, tmp->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
-		tmp = tmp->next;
+		h = h->next;
+		count++;
 	}
-	return (i);
+	return (count);
 }
