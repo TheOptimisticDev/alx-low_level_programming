1c48f6cb4c64b4acbd1740b94ce4b8a8202786f9
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-12-06 03:31:33 -0800

    hot fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,11 +21,10 @@ typedef struct list_s
 	struct list_s *next;
 } lists_t;
 
-int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif
