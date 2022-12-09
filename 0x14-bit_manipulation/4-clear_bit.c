78bee0375a68527d972de1d2b028f8420d7f96b1
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-12-09 02:48:56 -0800

    fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * clear_bit - sets a bit at a position to 0
- * @n: number to be used
- * @index: position to be cleared
- *
- * Return: 1 on success, -1 on fail
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: number.
+ * @index: index, starting from 0 of the bit you want to set.
+ * Return: 1 if it worked, or -1 if an error occurred.
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
