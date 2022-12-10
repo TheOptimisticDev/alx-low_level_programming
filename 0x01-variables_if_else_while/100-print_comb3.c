745b9970fdad863ebfde58210caa14f393c60d55
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-12-10 11:48:33 -0800

    Advancedtask

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,24 @@
 
 int main(void)
 {
-	int i, j;
+	int digit1, digit2;
 
-	for (i = 0; i <= 9; i++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for j = 1; j <= 9; j++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			if (j > i)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
