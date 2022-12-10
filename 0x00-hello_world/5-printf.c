684499d79339705586721ac6e7658c68c4a6a56c
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-12-10 05:24:08 -0800

    re-do

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main -Entry point
+ * my hello , print task
+ * Return: return 1 after program execution
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
