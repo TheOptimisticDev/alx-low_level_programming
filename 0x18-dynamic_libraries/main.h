95bf19bafca8fc7067f9c2c0a056056a331be717
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-03-03 23:47:17 -0800

    commit

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef DYNAMIC_LIB
+#define DYNAMIC_LIB
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* _MAIN_H_ */
+#endif /* DYNAMIC_LIB */
