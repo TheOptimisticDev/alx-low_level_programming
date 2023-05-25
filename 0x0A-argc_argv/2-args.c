0987cf5ad01169569b4c64381d3b933ffae27a2e
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-05-25 05:34:38 -0700

    Advanced Task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - Program that prints the number of arguments passed into it
- * @argc: Int of arguments passed into program including command
- * @argv: Array of pointers to the strings of arguments passed
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; argv[i] != '\0'; argc--, i++)
-		printf("%s\n", argv[i]);
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
 
 	return (0);
 }
