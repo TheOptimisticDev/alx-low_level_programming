6bf115e543c2b80d1e9b5da6048bc55e33162050
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-06-28 06:06:32 -0700

    correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,15 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL )
-		return;
-
-	if ((*d).name == NULL)
-		printf("nil\n");
-	else
-		printf("Name: %s\n", d->name);
-
-	if ((*d).age < 0)
-		printf("nil\n");
-	else
+	if (d)
+	{
+		/* I print as the example */
+		/* I use the ? operator */
+		/* return its left-hand operand if is not null */
+		/* otherwise,evaluates the righ operand and return its result*/
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		/* compiler doesn't let me add condition for the float type */
 		printf("Age: %f\n", d->age);
-
-	if ((*d).owner == NULL)
-		printf("nil\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
 }
